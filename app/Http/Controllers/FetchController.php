e4915f9bdc0231abce622d885c4198ce87aea749
okandrms <okanndrms@gmail.com>
2024-05-27 13:23:31 +0200

    some bugs fixed

--- app/Http/Controllers/FetchController.php
+++ app/Http/Controllers/FetchController.php
@@ -3,12 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
+
 
 class FetchController extends Controller
 {
     public static function getBitcoinPrice()
     {
-        $json = file_get_contents('https://api.coindesk.com/v1/bpi/currentprice.json');
+        $json = Http::get('https://api.coindesk.com/v1/bpi/currentprice.json');
         $data = json_decode($json, true);
         // dd($data);
         return $data['bpi']['EUR']['rate_float'];
